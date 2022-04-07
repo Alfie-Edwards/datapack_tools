43c482e1635cc441c7bcf5c8d25dd5d240bbb9f4
Alfie Edwards <alfie-edwards@hotmail.com>
2022-04-07 13:32:56 +0100

    Adding type test functions

--- data_types.py
+++ data_types.py
@@ -1,28 +1,100 @@
 import random
 import builtins
+import re
+
+# Type test functions.
+
+def _is_string_matching(x, pattern):
+   return isinstance(x, builtins.str) and re.match(pattern, x)
+
+def is_byte(x):
+   return _is_string_matching(x, r"^\\d+[bB]$")
+
+def is_short(x):
+   return _is_string_matching(x, r"^\d+[sS]$")
+
+def is_int(x):
+   return _is_string_matching(x, r"^\d+$")
+
+def is_long(x):
+   return _is_string_matching(x, r"^\d+[lL]$")
+
+def is_float(x):
+   return _is_string_matching(x, r"^\d+[fF]$")
+
+def is_double(x):
+   return _is_string_matching(x, r"^\d+[dD]$")
+
+def is_byte_array(x):
+   return _is_string_matching(x, r"^\[B;((\d+[bB],)*\d+[bB])?\]$")
+
+def is_short_array(x):
+   return _is_string_matching(x, r"^\[S;((\d+[sS],)*\d+[sS])?\]$")
+
+def is_int_array(x):
+   return _is_string_matching(x, r"^\[I;((\d+,)*\d+)?\]$")
+
+def is_long_array(x):
+   return _is_string_matching(x, r"^\[L;((\d+[lL],)*\d+[lL])?\]$")
+
+def is_string(x):
+   return isinstance(x, builtins.str)
+
+_TYPE_TEST_FUNCTIONS = [
+   is_byte
+   is_short
+   is_int
+   is_long
+   is_float
+   is_double
+   is_byte_array
+   is_short_array
+   is_int_array
+   is_long_array
+   is_string
+]
+
+# Types.
 
 def uuid():
    return int_array(random.randrange(-(2**31),2**31) for _ in range(4))
 
 def byte(b):
+   if is_byte(b):
+      return b
    return f"{builtins.int(b)}b"
 
+TRUE = byte(1)
+FALSE = byte(0)
+
 def bool(b):
+   if is_byte(b):
+      return b
    return TRUE if b else FALSE
 
 def short(s):
+   if _is_string_matching(s, r"^\\d+s$"):
+      return s
    return f"{builtins.int(s)}s"
 
 def int(i):
+   if is_int(i):
+      return i
    return str(builtins.int(i))
 
 def long(l):
-   return f"{builtins.int(s)}l"
+   if is_long(l):
+      return l
+   return f"{builtins.int(l)}l"
 
 def float(f):
+   if is_float(f):
+      return f
    return f"{builtins.float(f)}f"
 
 def double(d):
+   if _is_string_matching(d, r"^\\d+d$"):
+      return d
    return f"{builtins.float(d)}d"
 
 def byte_array(*bytes):
@@ -49,17 +121,17 @@ def string(s):
    return f'"{s}"'
 
 def auto(value):
-   if isinstance(value, str):
-      return string(value)
-   if isinstance(value, bool):
+   if isinstance(value, builtins.bool):
       return bool(value)
-   if isinstance(value, int):
+   if isinstance(value, builtins.int):
       return int(value)
-   if isinstance(value, float):
+   if isinstance(value, builtins.float):
       return float(value)
+   if any(f(value) for f in _TYPE_TEST_FUNCTIONS):
+      return value
    raise ValueError(f"Unable to determine a minecraft json type for {type(value)}")
 
-# Constants
+# Constants.
 
 def _min_signed(bits):
    return -(2 ** (bits - 1))
@@ -67,8 +139,6 @@ def _min_signed(bits):
 def _max_signed(bits):
    return (2 ** (bits - 1)) - 1
 
-TRUE = byte(1)
-FALSE = byte(0)
 MAX_INT = _max_signed(64)
 MIN_INT = _min_signed(64)
 MAX_INT = _max_signed(32)
